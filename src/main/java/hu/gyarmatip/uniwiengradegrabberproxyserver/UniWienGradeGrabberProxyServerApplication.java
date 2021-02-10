80cde6f6fb53f6853c94f497f8ef45ae4f1e5b9d
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2021-02-10 15:45:18 +0100

    initial commit

---
+++ src/main/java/hu/gyarmatip/uniwiengradegrabberproxyserver/UniWienGradeGrabberProxyServerApplication.java
@@ -0,0 +1,13 @@
+package hu.gyarmatip.uniwiengradegrabberproxyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniWienGradeGrabberProxyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniWienGradeGrabberProxyServerApplication.class, args);
+    }
+
+}
